5bc542df08d2aa2113cc45272a0ae2be6c5086a5
Darren Beattie <darren@fettle.ca>
2020-05-21 10:27:16 -0700

    fix reducer to also delete stored numbers in state on click

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -31,11 +31,18 @@ const reducer = (state = initialState, action) => {
         ...state,
         results: state.results.concat({id: new Date(), value: state.counter})
       }
-    // case 'DELETE_RESULT':
-    //   return {
-    //     ...state,
-    //     results: state.results.concat(state.counter)
-    //   }
+    case 'DELETE_RESULT':
+      //METHOD 1 for removing something from an Array immutably
+      // const id = 2;
+      // const newArr = [...state.results]
+      // newArray.slice(id, 1)
+      
+      //METHOD 2 filter method
+      const updatedArr = state.results.filter(result => result.id !== action.resultElementId);
+      return {
+        ...state,
+        results: updatedArr
+      }
   }
   return state;
 };
