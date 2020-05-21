fa8dcc947898b8152a615985268dc69d8eac767c
Darren Beattie <darren@fettle.ca>
2020-05-20 17:51:16 -0700

    add decrement, add and subtract 5 to counter via reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,6 +9,21 @@ const reducer = (state = initialState, action) => {
       counter: state.counter + 1
     }
   }
+  if (action.type === 'DECREMENT') {
+    return {
+      counter: state.counter - 1
+    }
+  }
+  if (action.type === 'ADD_FIVE') {
+    return {
+      counter: state.counter + 5
+    }
+  }
+  if (action.type === 'SUBTRACT_FIVE') {
+    return {
+      counter: state.counter - 5
+    }
+  }
   return state;
 }
 
